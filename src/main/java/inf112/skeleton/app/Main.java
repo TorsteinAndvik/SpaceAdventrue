64c5e5aa7fbca70c7906d22a3a3fceb74ae758f2
Flemming.Bjørdal <flemming.bjordal@uib.no>
2025-02-24 14:44:40 +0100

    Adjust grid tile pngs, change window size to accomodate

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		}
 		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
 		cfg.setTitle("TestSpaceGame");
-        cfg.setWindowedMode(1224, 1224);
+        cfg.setWindowedMode(2*9*72, 2*9*72);
 		new Lwjgl3Application(new TestSpaceGame(), cfg);
 	}
 }
