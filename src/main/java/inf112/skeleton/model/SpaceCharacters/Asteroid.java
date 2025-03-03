330a34e9ddcc11c1f168a90fc3cf5162b65afa12
Torstein Andvik <toand8457@uib.no>
2025-03-03 23:07:54 +0100

    Substitute healthPoints with hitPoints

--- src/main/java/inf112/skeleton/model/SpaceCharacters/Asteroid.java
+++ src/main/java/inf112/skeleton/model/SpaceCharacters/Asteroid.java
@@ -2,8 +2,8 @@ package inf112.skeleton.model.SpaceCharacters;
 
 public class Asteroid extends Projectile {
 
-  public Asteroid(String name, String description, int x, int y, int healthPoints, int mass, int speed, float angle, int radius) {
-    super(name, description, x, y, healthPoints, mass, speed, angle, radius);
+  public Asteroid(String name, String description, int hitPoints, int x, int y, int mass, int speed, float angle, int radius) {
+    super(name, description, x, y, hitPoints, mass, speed, angle, radius);
     //TODO Auto-generated constructor stub
   }
 
