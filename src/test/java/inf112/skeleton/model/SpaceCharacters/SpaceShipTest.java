1ded497777c654a48313aa4fc0020e2f8a33adf6
Torstein Andvik <toand8457@uib.no>
2025-03-03 23:08:50 +0100

    Add tests for SpaceShip

---
+++ src/test/java/inf112/skeleton/model/SpaceCharacters/SpaceShipTest.java
@@ -0,0 +1,56 @@
+package inf112.skeleton.model.SpaceCharacters;
+
+import inf112.skeleton.model.Globals.DamageDealer;
+import inf112.skeleton.model.Globals.Damageable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpaceShipTest {
+  private SpaceShip spaceShip;
+
+  @BeforeEach()
+  void setup() {
+    spaceShip = new Player("The Black Swan", "A beautiful pirate ship.",
+            100, 0, 0, 100, 40, 53, 60);
+  }
+
+  @Test
+  void takeDamageTest() {
+    assertEquals(100, spaceShip.getHitPoints());
+    assertEquals(100, spaceShip.getMaxHitPoints());
+    spaceShip.takeDamage(50);
+    assertEquals(50, spaceShip.getHitPoints());
+    assertEquals(100, spaceShip.getMaxHitPoints());
+
+  }
+
+  @Test
+  void repairTest() {
+    assertEquals(100, spaceShip.getHitPoints());
+    spaceShip.takeDamage(99);
+    assertEquals(1, spaceShip.getHitPoints());
+    spaceShip.repair(1);
+    assertEquals(2, spaceShip.getHitPoints());
+    spaceShip.repair(2000);
+    assertEquals(100, spaceShip.getHitPoints());
+  }
+
+  @Test
+  void destroyedTest() {
+    assertFalse(spaceShip.isDestroyed());
+    spaceShip.takeDamage(100);
+    assertTrue(spaceShip.isDestroyed());
+  }
+
+  @Test
+  void dealDamageTest() {
+    SpaceShip enemyShip = new Player("The White Swan", "A boring ship.",
+            49, 0, 0, 100, 40, 53, 60);
+    assertEquals(49, enemyShip.getHitPoints());
+    spaceShip.dealDamage(enemyShip);
+    assertEquals(51, spaceShip.getHitPoints());
+  }
+
+}
