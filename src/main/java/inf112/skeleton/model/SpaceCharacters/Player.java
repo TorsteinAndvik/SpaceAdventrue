f3927109f7f7b4aef568cb458c8f044b7e17d7c7
Torstein Andvik <toand8457@uib.no>
2025-03-04 01:03:52 +0100

    Add radius to Player

--- src/main/java/inf112/skeleton/model/SpaceCharacters/Player.java
+++ src/main/java/inf112/skeleton/model/SpaceCharacters/Player.java
@@ -2,8 +2,7 @@ package inf112.skeleton.model.SpaceCharacters;
 
 public class Player extends SpaceShip {
 
-
-  public Player(String name, String description, int healthPoints, int x, int y, int mass, int speed, float angle) {
-    super(name, description, healthPoints, x, y, mass, speed, angle);
+  public Player(String name, String description, int hitPoints, int x, int y, int mass, int speed, float angle, int radius) {
+    super(name, description, hitPoints, x, y, mass, speed, angle, radius);
   }
 }
