6df64f4c7989f8585c21a14ae0d596033c90b680
Torstein Andvik <toand8457@uib.no>
2025-03-09 00:37:02 +0100

    Fix changes in arguments

--- src/test/java/inf112/skeleton/model/SpaceCharacters/SpaceBodyTest.java
+++ src/test/java/inf112/skeleton/model/SpaceCharacters/SpaceBodyTest.java
@@ -10,11 +10,13 @@ public class SpaceBodyTest {
   void createSpaceBodyTest() {
     SpaceBody asteroid = new Asteroid(
             "Ceres", "Ceres is a dwarf planet",
-            100, 37, 68, 600, 45, 200);
+            100, 37, 0, 17, 68, 600, 45, 200);
     assertEquals("Ceres", asteroid.getName());
     assertEquals("Ceres is a dwarf planet", asteroid.getDescription());
     assertEquals(37, asteroid.getX());
     assertEquals(68, asteroid.getY());
+    assertEquals(0, asteroid.getVelocity().x);
+    assertEquals(17, asteroid.getVelocity().y);
     assertEquals(600, asteroid.getMass());
     assertEquals(45, asteroid.getRotationAngle());
     assertEquals(200, asteroid.getRadius());
@@ -25,7 +27,7 @@ public class SpaceBodyTest {
   void ChangeAngleSpaceBodyTest() {
     SpaceBody asteroid = new Asteroid(
             "Ceres", "Ceres is a dwarf planet",
-            100, 0, 1, 1, 45, 100);
+            100, 0, 0, 0, 1, 1, 45, 100);
     assertEquals(45, asteroid.getRotationAngle());
     asteroid.rotate(314);
     assertEquals(359, asteroid.getRotationAngle());
@@ -38,11 +40,13 @@ public class SpaceBodyTest {
   void SetterGetterTests() {
     SpaceBody asteroid = new Asteroid(
             "Ceres", "Ceres is a dwarf planet",
-            100, 68, 100, 600, 45, 100);
+            100, 68, 0, 0, 100, 600, 45, 100);
     asteroid.setName("Hygiea");
     asteroid.setDescription("Hygiea is a major asteroid located in the main asteroid belt");
     asteroid.setX(0);
     asteroid.setY(1);
+    asteroid.setVelocityX(2);
+    asteroid.setVelocityY(3);
     asteroid.setMass(500);
     asteroid.setRotation(90);
     asteroid.setRadius(200);
@@ -50,6 +54,8 @@ public class SpaceBodyTest {
     assertEquals("Hygiea is a major asteroid located in the main asteroid belt", asteroid.getDescription());
     assertEquals(0, asteroid.getX());
     assertEquals(1, asteroid.getY());
+    assertEquals(2, asteroid.getVelocity().x);
+    assertEquals(3, asteroid.getVelocity().y);
     assertEquals(500, asteroid.getMass());
     assertEquals(90, asteroid.getRotationAngle());
     assertEquals(200, asteroid.getRadius());
