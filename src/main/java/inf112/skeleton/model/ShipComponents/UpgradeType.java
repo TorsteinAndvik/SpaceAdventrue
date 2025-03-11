129f61ee959ce1c8e43a5f4a77f6a74c8ffec6e6
Torstein Andvik <toand8457@uib.no>
2025-03-11 01:09:57 +0100

    Add getUpgradeType method

--- src/main/java/inf112/skeleton/model/ShipComponents/UpgradeType.java
+++ src/main/java/inf112/skeleton/model/ShipComponents/UpgradeType.java
@@ -1,7 +1,15 @@
 package inf112.skeleton.model.ShipComponents;
 
 public enum UpgradeType {
-    THRUSTER,
-    TURRET,
-    SHIELD;
+  THRUSTER,
+  TURRET,
+  SHIELD;
+
+  public static UpgradeType getUpgradeType(String name) {
+    try {
+      return UpgradeType.valueOf(name.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException("Invalid upgrade type: " + name);
+    }
+  }
 }
