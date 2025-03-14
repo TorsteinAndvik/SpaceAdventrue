659a1daacd1132392933656463c2b7a0c666f7eb
Henrik Dalsto <henrik.dalsto@hvl.no>
2025-03-14 21:06:41 +0100

    add shield test

--- src/test/java/inf112/skeleton/model/ShipComponents/Components/ShieldTest.java
+++ src/test/java/inf112/skeleton/model/ShipComponents/Components/ShieldTest.java
@@ -1,5 +1,31 @@
 package inf112.skeleton.model.ShipComponents.Components;
 
+import inf112.skeleton.model.ShipComponents.UpgradeStage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ShieldTest {
 
+    Shield shield;
+    Shield superShield;
+
+    @BeforeEach
+    void setUp() {
+        shield = new Shield();
+        superShield = new Shield(UpgradeStage.MAX);
+    }
+
+    @Test
+    void shieldTest() {
+        assertEquals("Shield", shield.getName());
+        assertEquals(UpgradeStage.ZERO, shield.getStage());
+    }
+
+    @Test
+    void superShieldTest() {
+        assertEquals("Shield", superShield.getName());
+        assertEquals(UpgradeStage.MAX, superShield.getStage());
+    }
 }
