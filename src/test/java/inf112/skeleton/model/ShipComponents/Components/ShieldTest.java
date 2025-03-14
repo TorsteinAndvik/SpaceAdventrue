57a4a980b1023df9b69b42e33a1c3c1cc8ed8be4
Henrik Dalsto <henrik.dalsto@hvl.no>
2025-03-14 21:13:37 +0100

    finish shield tests; make variables in ShipUpgrade final

--- src/test/java/inf112/skeleton/model/ShipComponents/Components/ShieldTest.java
+++ src/test/java/inf112/skeleton/model/ShipComponents/Components/ShieldTest.java
@@ -1,6 +1,7 @@
 package inf112.skeleton.model.ShipComponents.Components;
 
 import inf112.skeleton.model.ShipComponents.UpgradeStage;
+import inf112.skeleton.model.ShipComponents.UpgradeType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,11 +22,15 @@ class ShieldTest {
     void shieldTest() {
         assertEquals("Shield", shield.getName());
         assertEquals(UpgradeStage.ZERO, shield.getStage());
+        assertEquals("Reduces damage taken from enemies and collisions", shield.getDescription());
+        assertEquals(UpgradeType.SHIELD, shield.getType());
     }
 
     @Test
     void superShieldTest() {
         assertEquals("Shield", superShield.getName());
         assertEquals(UpgradeStage.MAX, superShield.getStage());
+        assertEquals("Reduces damage taken from enemies and collisions", shield.getDescription());
+        assertEquals(UpgradeType.SHIELD, shield.getType());
     }
 }
