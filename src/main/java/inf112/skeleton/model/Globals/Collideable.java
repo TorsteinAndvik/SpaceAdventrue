23f0d872a2ee450b67abb22cab6598f9140691f6
TorInf101 <two006@uib.no>
2025-03-21 14:32:36 +0100

    Moving Collideable

---
+++ src/main/java/inf112/skeleton/model/Globals/Collideable.java
@@ -0,0 +1,24 @@
+package inf112.skeleton.model.Globals;
+
+
+public interface Collideable {
+
+
+    /**
+   * @return the x coordinate of the collideable object
+   */
+  int getX();
+
+  /**
+   * @return the x coordinate of the collideable object
+   */
+  int getY();
+
+
+  /**
+   * @return the radius of the collideable object
+   */
+  int getRadius();
+
+
+}
