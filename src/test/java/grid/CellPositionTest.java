0948159e762e540e5bb5755706e380e08f4a2225
Torstein Andvik <toand8457@uib.no>
2025-03-30 23:35:34 +0200

    Add offset method

--- src/test/java/grid/CellPositionTest.java
+++ src/test/java/grid/CellPositionTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
-import grid.CellPosition;
 import org.junit.jupiter.api.Test;
 
 class CellPositionTest {
@@ -43,5 +42,20 @@ class CellPositionTest {
         CellPosition c = new CellPosition(1, 2);
         CellPosition d = new CellPosition(1, 2);
         assertEquals(c.hashCode(), d.hashCode());
+    }
+
+    @Test
+    void offsetTest() {
+        CellPosition a = new CellPosition(0, 1);
+        CellPosition b = a.offset(1, 1);
+        assertEquals(0, a.row());
+        assertEquals(1, a.col());
+
+        assertEquals(1, b.row());
+        assertEquals(2, b.col());
+
+        CellPosition c = a.offset(-1, -1);
+        assertEquals(-1, c.row());
+        assertEquals(0, c.col());
     }
 }
