5dba855af345c3b08c337ca080ba2ba792923a2a
Flemming.Bjørdal <flemming.bjordal@uib.no>
2025-04-05 18:45:33 +0200

    add foundational code to HealthBar

--- src/main/java/view/bars/HealthBar.java
+++ src/main/java/view/bars/HealthBar.java
@@ -1,13 +1,44 @@
 package view.bars;
 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 import model.Globals.Damageable;
+import model.utils.FloatPair;
 
 public class HealthBar extends PercentageBar {
     Damageable subject;
+    FloatPair offset;
 
-    public HealthBar(Damageable subject) {
+    public HealthBar(Damageable subject, FloatPair offset) {
         super(subject.getMaxHitPoints(), subject.getHitPoints());
         this.subject = subject;
+        this.offset = offset;
     }
 
+    @Override
+    public void setPosition(FloatPair pos) {
+        this.setPosition(pos.x() + offset.x(), pos.y() + offset.y());
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        super.setPosition(x + offset.x(), y + offset.y());
+    }
+
+    @Override
+    public void setCenter(FloatPair pos) {
+        this.setCenter(pos.x() + offset.x(), pos.y() + offset.y());
+    }
+
+    @Override
+    public void setCenter(float x, float y) {
+        super.setCenter(x + offset.x(), y + offset.y());
+    }
+
+    @Override
+    public void draw(ShapeRenderer renderer) {
+        setMaxValue(subject.getMaxHitPoints());
+        setCurrentValue(subject.getHitPoints());
+        super.draw(renderer);
+    }
 }
