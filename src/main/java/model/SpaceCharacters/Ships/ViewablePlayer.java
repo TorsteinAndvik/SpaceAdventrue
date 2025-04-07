68911649e51d67ce80ee31ae032333eef36f0f0b
Torstein Andvik <toand8457@uib.no>
2025-04-08 00:37:56 +0200

    Add view methods

---
+++ src/main/java/model/SpaceCharacters/Ships/ViewablePlayer.java
@@ -0,0 +1,23 @@
+package model.SpaceCharacters.Ships;
+
+import model.ShipComponents.Components.ShipStructure;
+import model.ViewableSpaceShip;
+
+public interface ViewablePlayer extends ViewableSpaceShip {
+
+    /**
+     * Returns the structure of the player's ship.
+     *
+     * @return the {@link ShipStructure} representing the current state of the ship's structure
+     */
+    ShipStructure getShipStructure();
+
+    /**
+     * Returns the current inventory of the player’s ship. This includes items
+     * like cargo, upgrades, and collectibles.
+     *
+     * @return the {@link ViewableInventory} associated with the player
+     */
+    ViewableInventory getInventory();
+
+}
