88ddf9ee68cd59eac80809275247cf45e9759065
Henrik Dalsto <henrik.dalsto@hvl.no>
2025-04-13 15:03:33 +0200

    add controller functions; methods to be implemented

--- src/main/java/controller/OptionsScreenController.java
+++ src/main/java/controller/OptionsScreenController.java
@@ -1,16 +1,120 @@
 package controller;
 
+import app.TestSpaceGame;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector3;
+import controller.audio.SoundEffect;
+import java.util.List;
 import model.GameStateModel;
+import model.constants.GameState;
+import model.utils.MenuButton;
 import view.SpaceGame;
 import view.screens.OptionsScreen;
 
 public class OptionsScreenController extends GenericController {
 
+    private final OptionsScreen view;
 
     public OptionsScreenController(OptionsScreen view, GameStateModel model, SpaceGame game) {
         super(view, model, game);
+        this.view = view;
 
     }
+
+    @Override
+    protected boolean handleKeyDown(int keyCode) {
+        List<MenuButton> buttons = view.getOptionButtons();
+        if (buttons.isEmpty()) {
+            return false;
+        }
+
+        switch (keyCode) {
+            case Input.Keys.UP:
+                gameStateModel.setSelectedButtonIndex(
+                    Math.max(0, gameStateModel.getSelectedButtonIndex() - 1));
+                soundManager.play(SoundEffect.MENU_SELECT, 0.4f);
+                return true;
+            case Input.Keys.DOWN:
+                gameStateModel.setSelectedButtonIndex(
+                    Math.max(0, gameStateModel.getSelectedButtonIndex() + 1));
+                soundManager.play(SoundEffect.MENU_SELECT, 0.4f);
+                return true;
+            case Input.Keys.ENTER:
+            case Input.Keys.SPACE:
+                activateButton(gameStateModel.getSelectedButtonIndex());
+                return true;
+            case Input.Keys.ESCAPE:
+            case Input.Keys.P:
+                returnToPreviousScreen();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector3 worldCoords = view.unprojectScreenCoords(screenX, screenY);
+
+        //Check for button clicks
+        List<MenuButton> buttons = view.getOptionButtons();
+        for (int i = 0; i < buttons.size(); i++) {
+            if (buttons.get(i).getBounds().contains(worldCoords.x, worldCoords.y)) {
+                gameStateModel.setSelectedButtonIndex(i);
+                activateButton(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void activateButton(int buttonIndex) {
+        soundManager.play(SoundEffect.MENU_SELECT, 0.7f);
+        List<MenuButton> buttons = view.getOptionButtons();
+
+        switch (buttonIndex) {
+            case 0:
+                toggleSound();
+                break;
+            case 1:
+                toggleMusic();
+                break;
+            case 2:
+                returnToPreviousScreen();
+                break;
+        }
+    }
+
+    private void toggleSound() {
+        boolean soundEnabled = soundManager.isSoundEnabled();
+        soundManager.setSoundEnabled(!soundEnabled);
+
+        //Update button text
+        List<MenuButton> buttons = view.getOptionButtons();
+        MenuButton soundButton = buttons.get(0);
+        soundButton.setText("SOUND: " + (soundManager.isSoundEnabled() ? "ON" : "OFF"));
+    }
+
+    private void toggleMusic() {
+        boolean musicEnabled = musicManager.isMusicEnabled();
+        musicManager.setMusicEnabled(!musicEnabled);
+
+        //Update button text
+        List<MenuButton> buttons = view.getOptionButtons();
+        MenuButton musicButton = buttons.get(0);
+        musicButton.setText("MUSIC: " + (musicManager.isMusicEnabled() ? "ON" : "OFF"));
+    }
+
+    private void returnToPreviousScreen() {
+        GameState prevState = gameStateModel.getPreviousState();
+        if (prevState == GameState.START_GAME) {
+            ((TestSpaceGame) game).setStartScreen();
+        } else if (prevState == GameState.PLAYING) {
+            game.setSpaceScreen();
+        } else {
+            ((TestSpaceGame) game).setStartScreen();
+        }
+    }
+
 
     @Override
     public void update(float delta) {
@@ -19,7 +123,7 @@ public class OptionsScreenController extends GenericController {
 
     @Override
     protected boolean leftClick(int screenX, int screenY) {
-        return false;
+        return touchDown(screenX, screenY, 0, 0);
     }
 
     @Override
@@ -55,5 +159,24 @@ public class OptionsScreenController extends GenericController {
     @Override
     protected void handleScroll(float amountY) {
 
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        Vector3 worldCoords = view.unprojectScreenCoords(screenX, screenY);
+
+        //check hover over buttons
+        List<MenuButton> buttons = view.getOptionButtons();
+        for (int i = 0; i < buttons.size(); i++) {
+            if (buttons.get(i).getBounds().contains(worldCoords.x, worldCoords.y)) {
+                if (gameStateModel.getSelectedButtonIndex() != i) {
+                    gameStateModel.setSelectedButtonIndex(i);
+                    soundManager.play(SoundEffect.MENU_SELECT, 0.2f);
+                }
+                return true;
+            }
+
+        }
+        return false;
     }
 }
