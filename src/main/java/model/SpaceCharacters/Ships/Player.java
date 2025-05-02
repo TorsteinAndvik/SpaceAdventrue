395381e9de19e0a051d367c814cc49ec9fda91f4
Torstein Andvik <toand8457@uib.no>
2025-05-02 13:36:40 +0200

    Get rich faster

--- src/main/java/model/SpaceCharacters/Ships/Player.java
+++ src/main/java/model/SpaceCharacters/Ships/Player.java
@@ -16,7 +16,7 @@ public class Player extends SpaceShip implements ViewablePlayer {
 
     public Player(ShipStructure shipStructure, String name, String description, float x, float y) {
         super(shipStructure, name, description, CharacterType.PLAYER, x, y, 0);
-        inventory = new PlayerInventory(300);
+        inventory = new PlayerInventory(500);
     }
 
     @Override
